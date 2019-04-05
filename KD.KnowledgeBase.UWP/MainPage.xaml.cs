dd2e6d6d80db386bfaba5eba60ec89fcba9181dd
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2019-04-05 18:17:02 +0200

    Added removing the records;

--- KD.KnowledgeBase.UWP/MainPage.xaml.cs
+++ KD.KnowledgeBase.UWP/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Windows.Foundation;
 using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace KD.KnowledgeBase.UWP
@@ -12,6 +13,20 @@ namespace KD.KnowledgeBase.UWP
 
             ApplicationView.PreferredLaunchViewSize = new Size(1000, 440);
             ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
+
+        private void ButtonSaveClick(object sender, RoutedEventArgs e)
+        {
+            // Add new record.
+        }
+
+        private void ButtonDeleteClick(object sender, RoutedEventArgs e)
+        {
+            var selectedItem = this.tableListView.SelectedItem;
+            if (selectedItem != null)
+            {
+                this.tableListView.Items.Remove(selectedItem);
+            }
         }
     }
 }
